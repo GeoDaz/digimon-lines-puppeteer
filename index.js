3699276d5512c838bfbd6fee3f971306060a75ad
GeoDaz <gfrydz2@gmail.com>
2025-07-05 09:10:11 +0200

    fix dl dl

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 	res.send('Server is running');
 });
 
-app.post('/digimon-lines/build', screenshot);
+app.post('/digimon-lines/build', (req, res) => screenshot(req, res));
 app.post('/digimon-lines/build/pokemon', (req, res) => screenshot(req, res, 'pokemon'));
 
 app.listen(PORT, () => {
